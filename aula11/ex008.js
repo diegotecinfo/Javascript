5d709dcf5134833ffd251ff72f14af1e69107eae
Diego Carvalho <diegocarvalho@MacBook-Air-de-Diego.local>
2019-12-10 22:09:29 -0300

    aula11-condicoes

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condicao simples!
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de seguranca!')
