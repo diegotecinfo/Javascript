e33c7ddf840d6e6822c966718f8dce573182741b
Diego Carvalho <diegocarvalho@MacBook-Air-de-Diego.local>
2019-12-24 11:43:52 -0300

    repeticoes-pt1

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+/*
+var c = 1
+while ( c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // tem a mesma função que c = c + 1
+}
+*/
+var c = 0
+do {
+    console.log(`Executando passo ${c}`)
+    c++
+} while(c <= 10)
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
