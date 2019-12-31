9b33f6d3a99f4af4afeca17c64e65d7c37502369
Diego Carvalho <diegocarvalho@MacBook-Air-de-Diego.local>
2019-12-30 22:31:00 -0300

    correcao exercicio contador

--- aula14ex/ex016/correcao.js
+++ aula14ex/ex016/correcao.js
@@ -1,21 +1,33 @@
-function calcular () {
+function calcular() {
     var ini = document.getElementById('txtini')
     var fim = document.getElementById('txtfim')
     var passo = document.getElementById('txtpasso')
     var res = document.getElementById('res')
 
-    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('[ERRO] Faltam dados')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        //alert('[ERRO] Faltam dados')
     } else {
         res.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c}`
+        if (p <= 0){
+            window.alert('Passo inválido, considerando PASSO 1')
+            p = 1
         }
+        if (i < f) {
+            //Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }    
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
-
-
 }
