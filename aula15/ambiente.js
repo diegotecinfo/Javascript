1aef20d254f357bef6cbb23f4b3ff592f9d4f419
Diego Carvalho <diegocarvalho@MacBook-Air-de-Diego.local>
2020-01-03 01:06:30 -0300

    iniciando vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,25 @@
+let num = [5, 8, 2, 9, 3]
+num.sort()
+console.log(num)
+
+//ACRESCENTANDO UM POSICAO COM VALOR 6
+num[5] = 6
+console.log(num)
+
+// ACRESCENTANDO UM VALOR SEMPRE NA ULTIMA POSICAO
+num.push(7)
+console.log(num)
+
+//VERIFICANDO QUANTAS POSICOES TEM NOSSO ARRAY
+console.log(`O array tem ${num.length} posicoes`)
+
+//EXIBINDO DETERMINADA POSICAO DO NOSO ARRAY
+console.log(`A primeira posicao do array é ${num[0]}`)
+console.log(`A segunda posicao do array é ${num[1]}`)
+console.log(`A terceira posicao do array é ${num[2]}`)
+console.log(`A quarta posicao do array é ${num[3]}`)
+
+//ORDENANDO NOSSO ARRAY POR ORDEM CRESCRENTE
+console.log(num)
+console.log(num.sort())
+
